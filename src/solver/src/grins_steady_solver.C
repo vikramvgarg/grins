732375ecc94d0418f02b3dca2a81cdc57d105dd6
Vikram <vikram.v.garg@gmail.com>
2015-08-16 22:03:46 -0500

    Minor

--- src/solver/src/grins_steady_solver.C
+++ src/solver/src/grins_steady_solver.C
@@ -71,7 +71,7 @@ namespace GRINS
     // BEGIN hacking (to set solver options)
     libMesh::NewtonSolver* newton_solver = new libMesh::NewtonSolver( *(system) );
     newton_solver->require_residual_reduction = false;
-    // Finite Residual Hack
+    // Finite Residual Hack begins
     //std::cout<<"Finite Residual Hack"<<std::endl;
     //newton_solver->require_finite_residual = false;
     // End Residual Hack
